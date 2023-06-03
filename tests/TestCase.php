1929b42c51da6498e53fbddfc6763e486450f3b9
Bradie Tilley <44430471+bradietilley@users.noreply.github.com>
2023-06-03 21:16:09 +0800

    chore: remove unused Laravel Service Provider

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,15 +6,4 @@ use Orchestra\Testbench\TestCase as TestbenchTestCase;
 
 abstract class TestCase extends TestbenchTestCase
 {
-    protected function setUp(): void
-    {
-        parent::setUp();
-    }
-
-    protected function getPackageProviders($app)
-    {
-        return [
-            \BradieTilley\Rules\Laravel\RuleServiceProvider::class,
-        ];
-    }
 }
