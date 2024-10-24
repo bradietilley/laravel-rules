dfc0baa4f82e09045f5d484f1585486bbcb85fa9
Bradie Tilley <bradie@hatchet.com.au>
2024-10-24 16:10:49 +0800

    fix test dataset named arguments

--- tests/Feature/Validator/ValidatorTest.php
+++ tests/Feature/Validator/ValidatorTest.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 use BradieTilley\Rules\Rule;
 use Illuminate\Support\Facades\Validator;
 
-test('a validator instance can use Rulesets for each field', function (array $input, string $error = null) {
+test('a validator instance can use Rulesets for each field', function (array $input, ?string $errors) {
     $validator = Validator::make($input, [
         'foo' => Rule::make()->bail()->required()->string()->min(2)->max(20),
         'bar' => Rule::make()->bail()->nullable()->string()->in('pending', 'active'),
@@ -13,16 +13,16 @@ test('a validator instance can use Rulesets for each field', function (array $in
     ]);
     $passes = $validator->passes();
 
-    if ($error === null) {
+    if ($errors === null) {
         expect($passes)->toBeTrue();
 
         return;
     }
 
     $errors = implode(',', array_keys($validator->errors()->messages()));
-    expect($errors)->toBe($error);
+    expect($errors)->toBe($errors);
 })->with([
-    'passes' => [
+    'passes' => fn () => [
         'input' => [
             'foo' => 'some value',
             'bar' => 'pending',
@@ -30,7 +30,7 @@ test('a validator instance can use Rulesets for each field', function (array $in
         ],
         'errors' => null,
     ],
-    'fails on foo' => [
+    'fails on foo' => fn () => [
         'input' => [
             'foo' => 123,
             'bar' => 'pending',
@@ -38,7 +38,7 @@ test('a validator instance can use Rulesets for each field', function (array $in
         ],
         'errors' => 'foo',
     ],
-    'fails on bar' => [
+    'fails on bar' => fn () => [
         'input' => [
             'foo' => 'some value',
             'bar' => 'complete',
@@ -46,7 +46,7 @@ test('a validator instance can use Rulesets for each field', function (array $in
         ],
         'errors' => 'bar',
     ],
-    'fails on baz' => [
+    'fails on baz' => fn () => [
         'input' => [
             'foo' => 'some value',
             'bar' => 'pending',
